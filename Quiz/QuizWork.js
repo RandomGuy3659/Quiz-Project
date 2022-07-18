296c471b5d5f13005a3d00ff290424d6cbff9ef2
Nathan Cochrane <79487879+RandomGuy3659@users.noreply.github.com>
2022-07-18 17:49:47 +1000

    Random response list

--- Quiz/QuizWork.js
+++ Quiz/QuizWork.js
@@ -9,9 +9,24 @@ var correctAnswers = 0;
 
 var askTotal = 0;
 
-let Question = {"What is 15 + 22?"};
-
 function Answer(answerText, accuracy) {
-	this.answerText = "37";
-	this.accuracy = true;
+	this.answerText = answerText;
+	this.accuracy = accuracy;
+};
+
+function answerList() {
+	//a
+};
+
+function Question(questionText, answerList[]) {
+	this.questionText = questionText;
+	this.answerList[] = answerList();
+};
+
+let wrongAnswerResponseList = ["nope.", "maybe that was a mis-click?", "Sorry, that was incorrect.", "rip" "Yeah... that tracks."];
+let goodAnswerResponseList = ["We take those!", "That will do.", "Correct. Great job!", "Good answer!", "Yeah, that tracks."];
+
+function Response(target) {
+	var index = Math.floor(Math.random() * target.length);
+	return target[index];
 };
