adc70eb5d59c2c79ecb650a2f022b470da1246d0
Nathan Cochrane <79487879+RandomGuy3659@users.noreply.github.com>
2022-07-18 17:27:09 +1000

    basics of Question and Answer

--- Quiz/QuizWork.js
+++ Quiz/QuizWork.js
@@ -1,10 +1,17 @@
 "use strict";
 console.log("QuizWork.js is working");
 
-let Questions = ["a", "b", "c"];
+let QuestionList = ["a", "b", "c"];
 
-let currentQuestion = 0;
+var currentQuestion = 0;
 
-let correctAnswers = 0;
+var correctAnswers = 0;
 
-let askTotal = 0;
+var askTotal = 0;
+
+let Question = {"What is 15 + 22?"};
+
+function Answer(answerText, accuracy) {
+	this.answerText = "37";
+	this.accuracy = true;
+};
