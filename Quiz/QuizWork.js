c3c9c14a7f4ffc523da87ef4ef66e8f7c27cd98b
Nathan Cochrane <79487879+RandomGuy3659@users.noreply.github.com>
2022-12-26 17:02:03 +1100

    Overhaul of the Javascript workings
    The script should change the main body of the page by functions triggered when the links into the script made text is clicked.

--- Quiz/QuizWork.js
+++ Quiz/QuizWork.js
@@ -17,9 +17,13 @@ let Answer3A = {text: "The 10th month.", verity: false};
 let Answer3B = {text: "A 'cephalopod,' a mollusc and an underwater predator.", verity: false};
 let Answer3C = {text: "The prefix for 'eight.'", verity: true};
 
-//var page = document.getElementById("page");
-//console.log("QuizWork.js page: ", page.innerHTML);
+//const pageContent = document.getElementById("page");
+//console.log("QuizWork.js page: ", pageContent.textContent);
 
+//QContent is the innerHTML stored to be used as questions.
+//const QContent1 = "<strong>Question 1:</strong><br/>'+<i>Question1.text</i>+'<br/><div><a onClick="CorrectAnswer();" style="cursor: pointer; cursor: hand;">Question1.Ans1.text</a></div><br/><div><a onClick="WrongAnswer();" style="cursor: pointer; cursor: hand;">Question1.Ans2.text</a></div><br/><div><a onClick="WrongAnswer();" style="cursor: pointer; cursor: hand;">Question1.Ans3.text</a></div>";
+//const QContent2 = "<strong>Question 2:</strong><br/>'+<i>Question2.text</i>+'<br/><div><a onClick="WrongAnswer();" style="cursor: pointer; cursor: hand;">Question2.Ans1.text</a></div><br/><div><a onClick="CorrectAnswer();" style="cursor: pointer; cursor: hand;">Question2.Ans2.text</a></div><br/><div><a onClick="WrongAnswer();" style="cursor: pointer; cursor: hand;">Question2.Ans3.text</a></div>";
+//const QContent3 = "<strong>Question 3:</strong><br/>'+<i>Question2.text</i>+'<br/><div><a onClick="WrongAnswer();" style="cursor: pointer; cursor: hand;">Question3.Ans1.text</a></div><br/><div><a onClick="WrongAnswer();" style="cursor: pointer; cursor: hand;">Question3.Ans2.text</a></div><br/><div><a onClick="CorrectAnswer();" style="cursor: pointer; cursor: hand;">Question3.Ans3.text</a></div>";
 
 function Question(text, Ans1, Ans2, Ans3) {
 	this.text = text;
@@ -38,22 +42,36 @@ function startQuiz() {
 	loadQuestion();
 }
 
+
 function loadQuestion() {
 	currentStage += 1;
 	if (currentStage == 1) {
-		//
+		//pageContent.innerHTML = QContent1;
 	} else if (currentStage == 2) {
-		//
+		//pageContent.innerHTML = QContent2;
 	} else if (currentStage == 3) {
-		//	
+		//pageContent.innerHTML = QContent3;
 	} else {
 		results();
 	}
 	//
 }
 
+function CorrectAnswer() {
+	//everything green
+	//correctAnswers ++;
+	//new line that on click goes to next question.
+	pageContent.innerHTML = pageContent.innerHTML + "<a onClick=";loadQuestion();">The answer is correct. Please click here to continue.</a>";
+}
+
+function WrongAnswer() {
+	//everything red
+	//new line that on click goes to next question.
+	pageContent.innerHTML += "<a onClick=";loadQuestion();">The answer is incorrect. Please click here to continue.</a>";
+ }
+
 function results() {
-	//
+		pageContent.innerHTML = "You have gotten " + correctAnswers + " out of 3 Questions right. <a onClick=";startQuiz();">Please click here to start again.</a>";
 }
 
 console.log("Question 1 is " + Question1.text + " It's answers are: " + Question1.Ans1.text + ", which is " + Question1.Ans1.verity + ", " + Question1.Ans2.text + ", which is " + Question1.Ans2.verity + ", and " + Question1.Ans3.text + ", which is " + Question1.Ans3.verity + ".");
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Quiz-Project
  Javascript practice
+
+Sites used as references:
+https://www.rgagnon.com/jsdetails/js-0069.html
+https://sebhastian.com/javascript-change-text-on-page/
