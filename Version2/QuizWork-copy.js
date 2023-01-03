8fb74e021dc4b14f1a03e3140d03eecc037c2445
Nathan Cochrane <79487879+RandomGuy3659@users.noreply.github.com>
2023-01-03 15:21:16 +1100

    Added the questions, some styling.

--- Version2/QuizWork-copy.js
+++ Version2/QuizWork-copy.js
@@ -5,6 +5,13 @@ var correctAnswers = 0;
 var totalStages = 3;
 var currentStage = 0;
 
+const main = document.getElementById('.main');
+const cover = document.getElementById('.cover');
+const q1 = document.getElementById('.q1');
+const q2 = document.getElementById('.q2');
+const q3 = document.getElementById('.q3');
+const result = document.getElementById('.result');
+
 let Answer1A = {text: 19, verity: true};
 let Answer1B = {text: 21, verity: false};
 let Answer1C = {text: 910, verity: false};
@@ -31,37 +38,57 @@ let Question3 = new Question("What is 'Octo?'", Answer3A, Answer3B, Answer3C);
 function toStart() {
 	correctAnswers = 0;
 	currentStage = 0;
+	cover.style.display = 'block';
+	q1.style.display = 'none';
+	q2.style.display = 'none';
+	q3.style.display = 'none';
+	result.style.display = 'none';
 }
-
 
 function nextPart() {
 	currentStage += 1;
 	if (currentStage == 1) {
-		//pageContent.innerHTML = QContent1;
+		cover.style.display = 'none';
+		q1.style.display = 'block';
+		q2.style.display = 'none';
+		q3.style.display = 'none';
+		result.style.display = 'none';
 	} else if (currentStage == 2) {
-		//pageContent.innerHTML = QContent2;
+		cover.style.display = 'none';
+		q1.style.display = 'none';
+		q2.style.display = 'block';
+		q3.style.display = 'none';
+		result.style.display = 'none';
 	} else if (currentStage == 3) {
-		//pageContent.innerHTML = QContent3;
+		cover.style.display = 'none';
+		q1.style.display = 'none';
+		q2.style.display = 'none';
+		q3.style.display = 'block';
+		result.style.display = 'none';
 	} else {
 		results();
 	}
-	//
 }
 
 function CorrectAnswer() {
-	//everything green
+	console.log("A correct answer has been chosen!");
+	main.style.backgroundColor = 'green';
 	correctAnswers ++;
 	nextPart();
 }
 
 function WrongAnswer() {
-	//everything red
-	//new line that on click goes to next question.
+	console.log("An incorrect answer has been chosen.");
+	main.style.backgroundColor = 'red';
 	nextPart();
 }
 
 function results() {
-		pageContent.innerHTML = "You have gotten " + correctAnswers + " out of 3 Questions right. <button type='button' onClick='startQuiz();'>Please click here to start again.</button>";
+	cover.style.display = 'none';
+	q1.style.display = 'none';
+	q2.style.display = 'none';
+	q3.style.display = 'none';
+	result.style.display = 'block';
 }
 
 console.log("Question 1 is " + Question1.text + " It's answers are: " + Question1.Ans1.text + ", which is " + Question1.Ans1.verity + ", " + Question1.Ans2.text + ", which is " + Question1.Ans2.verity + ", and " + Question1.Ans3.text + ", which is " + Question1.Ans3.verity + ".");
